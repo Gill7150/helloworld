6ad4b6c399f983832e7769f1456112182a673029
gaganpreet singh <gaganpreet@example.com>
2023-01-20 19:04:29 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Gaganpreet Singh Student ID: 164321218 Date: 20/01/2021
+*
+*  Online (Cyclic) URL: https://gray-determined-moth.cyclic.app
+*
+********************************************************************************/ 
 
 
 var HTTP_PORT = process.env.PORT || 8080;
